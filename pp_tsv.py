9bb6710c1ef581544853ad5ae34e41ed9b8c5883
Sayantan Ghosh <sayantan.iitb@gmail.com>
2020-02-06 23:18:17 +0530

    Added pp_csv.py and pp_tsv.py scripts to the repository

---
+++ pp_tsv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from __future__ import print_function
+import csv
+import sys
+
+def pretty_print_csv(inputfile):
+	f = open(inputfile)
+	data = csv.DictReader(f, delimiter='\t')
+	i = 0
+	for entry in data:
+		i = i + 1
+		print('\n')
+		print("Row Number :", i)
+		for key,value in entry.items():
+			print(key, ":", value)
+		print('\n')
+		print('#'*50)
+
+def main(inputfile):
+	pretty_print_csv(inputfile)
+
+if __name__ == '__main__':
+	main(sys.argv[1])
