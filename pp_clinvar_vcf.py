38412304fc6a81fc8271e95d36c3c00a50552d7b
Sayantan Ghosh <sayantan.iitb@gmail.com>
2020-03-06 17:51:02 +0530

    Created a script to pretty print Clinvar VCF files

---
+++ pp_clinvar_vcf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+"""
+@author - sayantan
+"""
+
+from __future__ import print_function
+import csv
+import sys
+
+def read_vcf(vcf_file):
+	f = open(vcf_file, 'r')
+	for line in f.readlines():
+		if line.startswith('##'):
+			continue
+		yield line
+
+def pretty_print_vcf(data):
+	i = 0
+	for entry in data:
+		i = i + 1
+		print('\n')
+		print("Variant number :", i)
+
+		for key,value in entry.items():
+			if key == 'INFO':
+				print(key,":")
+				info = value.split(';')
+				for item in info:
+					left, right = item.split('=')
+					print('\t',left, ":", right)
+			else:
+				print(key, ":", value)
+		print('\n')
+		print('#'*50)
+
+def main(input_file):
+	global data
+	data = csv.DictReader(read_vcf(input_file), delimiter='\t')
+	pretty_print_vcf(data)
+
+if __name__ == '__main__':
+	main(sys.argv[1])
+
